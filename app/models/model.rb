443190c7ea536b90c5bcfcf2f10933b26bc41663
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-06-16 03:57:46 +0000

    Automatically backed up by Learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,5 +1,6 @@
 class Dog
   
+<<<<<<< HEAD
   @@all = []
   
   attr_accessor :name, :breed, :age
@@ -14,6 +15,16 @@ class Dog
   def self.all
     @@all
   end
+=======
+  attr_accessor :name, :breed, :age
+  
+  def initialize
+    @name = name
+    @breed = breed
+    @age = age
+  end
+  
+>>>>>>> 638023742c21a1825ea8ad8fae64a4cd9a89afa9
 
   
 end
